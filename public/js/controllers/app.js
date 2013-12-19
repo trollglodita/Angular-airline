2d1c65be186b16835536025722b0f7f64b3b049f
trollglodita <1trollglodita@gmail.com>
2013-12-19 13:23:45 -0300

    Controlling scopes

--- public/js/controllers/app.js
+++ public/js/controllers/app.js
@@ -28,4 +28,10 @@ function AppCtrl ($scope) {
       ]
     }
   };
+
+  $scope.currentAirport = null;
+
+  $scope.setAirport = function (code) {
+    $scope.currentAirport = $scope.airports[code];
+  };
 }
