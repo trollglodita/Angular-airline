a5dcdc105992e37b12dc1c073b1b76b94402014f
trollglodita <1trollglodita@gmail.com>
2013-12-19 12:51:15 -0300

    Init, node modules, index started

---
+++ README.md
@@ -0,0 +1,4 @@
+# Angular.js Interface
+
+
+## Install Node.js, an run index.js. Then, all the work has being done in the public folder.
---
+++ public/js/controllers/app.js
@@ -0,0 +1,31 @@
+function AppCtrl ($scope) {
+  $scope.airports = {
+    "PDX": {
+      "code": "PDX",
+      "name": "Portland International Airport",
+      "city": "Portland",
+      "destinations": [
+        "LAX",
+        "SFO"
+      ]
+    },
+    "STL": {
+      "code": "STL",
+      "name": "Lambert-St. Louis International Airport",
+      "city": "St. Louis",
+      "destinations": [
+        "LAX",
+        "MKE"
+      ]
+    },
+    "MCI": {
+      "code": "MCI",
+      "name": "Kansas City International Airport",
+      "city": "Kansas City",
+      "destinations": [
+        "LAX",
+        "DFW"
+      ]
+    }
+  };
+}
